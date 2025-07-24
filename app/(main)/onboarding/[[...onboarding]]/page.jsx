98001073bf79af1fc595d7310ba13aa543bcda50
himanshu deval <himanshu.deval@emizainc.net>
2025-07-24 21:31:30 +0530

    Minor changes

--- app/(main)/onboarding/[[...onboarding]]/page.jsx
+++ app/(main)/onboarding/[[...onboarding]]/page.jsx
@@ -1,23 +1,27 @@
 "use client";
+
 import { OrganizationList, useOrganization } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 
-const Onboarding = () => {
+export default function Onboarding() {
   const { organization } = useOrganization();
   const router = useRouter();
 
   useEffect(() => {
-    if (organization && organization?.slug) {
+    if (organization) {
       router.push(`/organization/${organization.slug}`);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [organization]);
 
   return (
-    <div className="flex justify-center items-center pt-15">
-      <OrganizationList hidePersonal />
+    <div className="flex justify-center items-center pt-14">
+      <OrganizationList
+        hidePersonal
+        afterCreateOrganizationUrl="/organization/:slug"
+        afterSelectOrganizationUrl="/organization/:slug"
+      />
     </div>
   );
-};
-
-export default Onboarding;
+}
